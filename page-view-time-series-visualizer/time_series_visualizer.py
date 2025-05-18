481434cdc5971f4daefe03d1c1b3629c11250926
arnouxali <aliakbrmaramy@gmail.com>
2025-05-18 17:31:48 +0330

    Update time_series_visualizer.py

--- page-view-time-series-visualizer/time_series_visualizer.py
+++ page-view-time-series-visualizer/time_series_visualizer.py
@@ -1,8 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-from pandas.plotting import register_matplotlib_converters
-register_matplotlib_converters()
+
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
 df =pd.read_csv('fcc-forum-pageviews.csv',index_col='date',parse_dates=True)
